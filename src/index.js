42b25299a4f0393bf8ef5d5538728347db7ddbb5
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-11-19 23:33:39 -0500

    completes challenge #1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: red');console.log('%c HI', 'color: orange');console.log('%c HI', 'color: goldenrod');console.log('%c HI', 'color: green');console.log('%c HI', 'color: blue');console.log('%c HI', 'color: indigo');console.log('%c HI', 'color: violet');
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const imgContainer = document.querySelector('#dog-image-container');
+
+// CHALLENGE 1
+
+fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(data => data.message.forEach(e => {
+        const newImg = document.createElement('img');
+        newImg.setAttribute('src', `${e}`);
+        newImg.style.height = '150px';
+        imgContainer.append(newImg);
+    }));
+
